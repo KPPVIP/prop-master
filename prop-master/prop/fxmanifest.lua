7b3b9fc5289f4a5e734f519083275b1450ea3593
Guudz <Kilian1702@outlook.com>
2023-01-28 02:13:02 +0100

    Initial commit

---
+++ prop-master/prop/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+game 'gta5'
+description 'Mrw_Prop'
+
+author 'Morow'
+
+client_scripts {
+  'client/warmenu.lua',
+  'client/client.lua'
+}
+
+
+server_scripts {
+  '@mysql-async/lib/MySQL.lua',
+  'server/server.lua'
+}
